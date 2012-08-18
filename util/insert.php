821f60d15bb77065711eeb45703aa799549e9056
Isaac Chavez <jico@justjico.com>
2012-08-18 13:38:03 -0400

    added files merged local content

---
+++ util/insert.php
@@ -0,0 +1,49 @@
+<?php
+
+$file_name = "pathnet.Fsqrnk.exp.txt";
+
+$link = mysql_connect(
+  ':/Applications/MAMP/tmp/mysql/mysql.sock',
+  'root',
+  'root'
+);
+if (!$link) {
+    die('Not connected : ' . mysql_error());
+}
+
+// make foo the current db
+$db_selected = mysql_select_db('pathways', $link);
+if (!$db_selected) {
+    die ('Can\'t use foo : ' . mysql_error());
+}
+
+$query = "select id,name from pathway_names";
+$result = mysql_query($query);
+
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+
+$pathway_id = array();
+
+while ($row = mysql_fetch_assoc($result)) {
+    $pathway_id[$row['name']] = $row['id'];
+}
+
+$file_array = file($file_name);
+
+foreach ($file_array as $line_number=>$line)
+{
+// Handle the line
+    //echo $line_number. " -  " . $line;
+//Pathway.A	Genes.A	Pathway.B	Genes.B	Overlap	Overlap.metric	Correlation	Adj.Correlation	Adjacency
+    list($pathway_a,$genes_a,$pathway_b,$genes_b,$overlap,$overlap_metric,$correlation,$adj_correlation,$adjacency) = explode("\t",$line);
+    echo $line_number . " - ". $pathway_id[$pathway_a] . "=>" . $pathway_a . " : " .$pathway_id[$pathway_b] . "=>" . $pathway_b . "\n" ;  
+    $ins_query = "INSERT INTO pathways (PathwayA, PA_id, GenesA, PathwayB, PB_id, GenesB, Overlap, OverlapMetric, Correlation, AdjCorrelation, Adjacency)" .
+    " values ('".mysql_real_escape_string($pathway_a)."', $pathway_id[$pathway_a], $genes_a, '".mysql_real_escape_string($pathway_b)."', $pathway_id[$pathway_b], $genes_b, $overlap, $overlap_metric, $correlation, $adj_correlation, $adjacency) ";
+    $result = mysql_query($ins_query);
+if (!$result) {
+    die('Invalid query: ' . mysql_error() . $ins_query);
+}
+    
+}
